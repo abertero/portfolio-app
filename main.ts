fe0b801263c2748bba9c3be221add67d54b8ba9a
abertero <thescientist14@gmail.com>
2025-05-15 00:17:06 -0400

    Adds end of line to separate outputs

--- main.ts
+++ main.ts
@@ -38,6 +38,27 @@ function secondCase() {
     portfolio.rebalance();
 }
 
+function thirdCase() {
+    const stock1 = new Stock("APPL", 1500, 200);
+    const stock2 = new Stock("META", 2500, 400);
+    const stock3 = new Stock("GOGL", 4000, 100);
+    
+
+    const portfolio = new Portfolio({
+        "APPL": 0.4,
+        "FINT": 0.6,
+    });
+
+    portfolio.addStock(stock1);
+    portfolio.addStock(stock2);
+    portfolio.addStock(stock3);
+
+    portfolio.validate();
+
+    portfolio.rebalance();
+}
+
+
 console.log(`First case: APPL and META 40/60 with stocks`)
 firstCase();
 console.log(`End of first case\n\n`)
